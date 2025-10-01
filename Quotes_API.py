fac452c1ffe1e8816994f2f91db57ad2215a5215
zach <zachmonari002@gmail.com>
2025-10-01 11:07:29 +0300

    Added a few quotes

--- Quotes_API.py
+++ Quotes_API.py
@@ -9,7 +9,10 @@ quotes = [
     "In the middle of every difficulty lies opportunity. – Albert Einstein",
     "Success is not final, failure is not fatal: It is the courage to continue that counts. – Winston Churchill",
     "What you get by achieving your goals is not as important as what you become by achieving your goals. – Zig Ziglar",
-    "Happiness is not something ready made. It comes from your own actions. – Dalai Lama"
+    "Happiness is not something ready made. It comes from your own actions. – Dalai Lama",
+    "The best way to predict the future is to invent it. — Alan Kay",
+    "The tools of the future are being built by those willing to think beyond today. — Various authors",
+    "Technology is best when it brings people together. — Matt Mullenweg"
 ]
 
 @app.get("/")
